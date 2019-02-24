443083f53c968da09a4e5dd9e142fdeb7a10edc6
AnitaI <anita.irsevic@gmail.com>
2019-02-24 16:48:13 +1300

    Initial commit

---
+++ Grades/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Grades
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            GradeBook book = new GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(71);
+
+            GradeStatistics stats = book.ComputeStatistics();
+            Console.WriteLine($"Average grade: {stats.AverageGrade}");
+            Console.WriteLine($"Highest grade: {stats.HighestGrade}");
+            Console.WriteLine($"Lowest grade: {stats.LowestGrade}");
+
+
+        }
+    }
+}
---
+++ GradesTest/GradeBookTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Xunit;
+using Grades;
+
+
+namespace GradesTest
+{
+    public class GradeBookTests
+    { 
+        [Fact]
+        public void ComputesHighestGrade()
+        {
+            Grades.GradeBook book = new Grades.GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            Grades.GradeStatistics result = book.ComputeStatistics();
+            Assert.Equal(90, result.HighestGrade);
+
+        }
+
+        [Fact]
+        public void ComputesLowestGrade()
+        {
+            Grades.GradeBook book = new Grades.GradeBook();
+            book.AddGrade(10);
+            book.AddGrade(90);
+
+            Grades.GradeStatistics result = book.ComputeStatistics();
+            Assert.Equal(10, result.LowestGrade);
+
+        }
+
+        [Fact]
+        public void ComputesAverageGrade()
+        {
+            Grades.GradeBook book = new Grades.GradeBook();
+            book.AddGrade(91);
+            book.AddGrade(89.5f);
+            book.AddGrade(75);
+
+            Grades.GradeStatistics result = book.ComputeStatistics();
+            Assert.Equal(85.17, result.AverageGrade, 2);
+
+        }
+
+    }
+}
